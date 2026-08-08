cbb908433fe09d508805735d7463d633d0778786
neerajkushwah07 <neerajkushwah10010@gmail.com>
2026-08-08 19:42:17 +0530

    Add query parameter examples

--- app/main.py
+++ app/main.py
@@ -32,3 +32,36 @@ def student_info(student_id: int, course_name: str):
         "Course Name": course_name
     }
 
+
+@app.get("/users")
+def search_users(name: str):
+    return {
+        "search_name": name
+    }
+
+@app.get("/products")
+def products(search: str | None = None):
+    return {
+        "search": search
+    }
+
+@app.get("/students")
+def students(page: int = 1, limit: int = 10):
+    return {
+        "page": page,
+        "limit": limit
+    }
+
+@app.get("/products")
+def product_list(
+    search: str | None = None,
+    category: str | None = None,
+    page: int = 1,
+    limit: int = 10
+):
+    return {
+        "search": search,
+        "category": category,
+        "page": page,
+        "limit": limit
+    }
